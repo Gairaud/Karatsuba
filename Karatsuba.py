c254ba2d51ba9b694bcb55b55951ca0781c765e3
Kevin Flores García <kevinfg1413@gmail.com>
2019-05-25 19:48:36 -0600

    La __div__ la tengo que revisar, lo que hay en __sub__ comentado es para que sirva sin complemento

--- Karatsuba.py
+++ Karatsuba.py
@@ -13,9 +13,22 @@ from abc import ABC, abstractmethod
 Default_base = 10
 MAX_SIZE = 16
 
+@total_ordering
 class AbstractNum:
     @abstractmethod
-    def __add__() : pass
+    def __add__(self, other) : pass
+    @abstractmethod
+    def __mul__(self, other) : pass
+    @abstractmethod
+    def __div__(self, other) : pass
+    @abstractmethod
+    def __sub__(self, other) : pass
+    @abstractmethod
+    def __eq__(self,other) : pass
+    @abstractmethod
+    def __lt__(self, other) : pass
+    @abstractmethod
+    def __invert__(self) : pass 
     
 class Num(AbstractNum):
     def __init__(self, digits = 0, b = Default_base):
@@ -37,7 +50,15 @@ class Num(AbstractNum):
         
     def __repr__(self):
         return self.__str__()
+
+    def __len__(self):
+        return self.numDigits
         
+    def __getitem__(self, key):
+        if key > self.numDigits:
+            raise Exception("Overflow")
+        return self.num[(MAX_SIZE - 1) - (self.numDigits - key)]
+
     def __add__(self, other):  
         if self.base != other.base:
             raise Exception("Different Bases")
@@ -55,6 +76,7 @@ class Num(AbstractNum):
                     llevo = False
             result = result[::-1]
             return type(self)("".join(str(i) for i in result), self.base)
+
     def __mul__(self, other):
         if self.base != other.base:
             raise Exception("Different Bases")
@@ -80,7 +102,100 @@ class Num(AbstractNum):
                 result = result[::-1]
                 total = total +  type(self)("".join(str(i) for i in result), self.base)
             return total
-            
+
+    def __div__(self, other):
+        if self.base != other.base:
+            raise Exception("Different Bases")
+        """
+        else:
+            result = []
+            num = 0
+            for x in range(MAX_SIZE - self.numDigits, MAX_SIZE):
+                num += self.num[x]
+                aux_1 = type(self)(digits = num, b = self.base)
+                if  aux_1 >= other:
+                    y = x
+                    while y < MAX_SIZE:
+                        multiply = 1
+                        aux_2 = (type(self)(digits = multiply, b = self.base) * other                      
+                        while aux_2 <= aux_1: 
+                            multiply += 1
+                            aux_2 = (type(self)(digits = multiply, b = self.base) * other
+                        if aux_1 == aux_2 : result.append(multiply)
+                        else: 
+                            result.append(multiply-1)
+                            aux_2 = (type(self)(digits = multiply - 1, b = self.base) * other
+                        y += 1
+                        if y < MAX_SIZE : 
+                            aux_1 = (aux_1 - aux_2) 
+                            aux_1 << 1
+                            aux_1 = aux_1 + type(self)(digits=self.num[y], b = self.base)
+                    break
+            return type(self)("".join(str(i) for i in result), self.base)
+        """
+
+    def __sub__(self, other):
+        
+        if self.base != other.base:
+            raise Exception("Different Bases")
+        """
+        else:
+            if self.numDigits < other.numDigits : 
+                bigger = other.num
+                smaller = self.num
+                negative = True
+            else:
+                bigger = self.num
+                smaller = other.num
+                negative = False
+            result = []
+            prestado = False
+            for x in range(MAX_SIZE-1, -1, -1):
+                resta = bigger[x] - smaller[x]
+                if resta < 0 : prestado = True
+                if prestado:
+                    result.append(resta + self.base)
+                    bigger[x - 1] -= 1
+                else:
+                    result.append(resta)
+            result = result[::-1]
+            total = type(self)("".join(str(i) for i in result), self.base)
+            return ~total if negative else total
+        """
+        return self + ~other
+
+    def __invert__(self):
+        """
+        x = [ self.base - 1 - x for x in self.num ]
+        return type(self)("".join(str(i) for i in x), self.base) + type(self)(1, self.base)
+        """
+        result = type(self)(b = self.base)
+        result.num = [ self.base - 1 - x for x in self.num ]
+        aux = type(self)(1, self.base)
+        result = result + aux
+        return result
+
+    def __eq__(self, other):
+        if self.base != other.base:
+            raise Exception("Different Bases")
+        for x in range(MAX_SIZE-1,-1,-1):
+            if self.num[x] != other.num[x] : return False
+        return True
+
+    def __lt__(self, other):
+        if self.base != other.base:
+            raise Exception("Different Bases")
+        if self.numDigits > other.numDigits or self == other: return False
+        elif self.numDigits == other.numDigits:
+            for x in range(self.numDigits -1, -1, -1):
+                k = self.num[MAX_SIZE - x - 1]*10**x
+                y = other.num[MAX_SIZE - x - 1]*10**x
+                if k > y: return False
+        return True
+    
+    def __lshift__(self, position):
+        return self * type(self)(10**position, self.base)
+
 class Knum(Num):
     def __add__(self, other):
         return super().__add__(other)
@@ -90,17 +205,17 @@ if __name__=="__main__":
     print("-------- PRUEBAS INICIALES --------\n")
     print("\nSUMAS \n")
     
-    x = 45
-    y = "13"
-    base3, base4, base7, base9, base10 = 3, 4, 7, 9,10
-    a = Num(x, base10)
+    x = 362
+    y = "26"
+    base3, base4, base7, base9, base10 = 3, 4, 7, 9, 10
+    """ a = Num(x, base10)
     b = Num(y, base10)
     c = Num(x, base4)
     d = Num(y, base4)
     e = Knum(x, base7)
     f = Knum(y, base7)
     g = Knum(x, base9)
-    h = Knum(y, base9)
+    h = Knum(y, base9)"""
     i = Num(x,base10)
     j = Num(y,base10)
     """  print(f"a = {a}")
@@ -115,6 +230,6 @@ if __name__=="__main__":
     print(f"c+d = {a+b}")
     print(f"e+f = {a+b}")
     print(f"g+h = {a+b}")
-    print(f"i+j = {i+j}")"""
     print(f"i+j = {i+j}")
-    print(f"i*j = {i*j}")
+    print(f"i+j = {i+j}")"""
+    print(i/j)
