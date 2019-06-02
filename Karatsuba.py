cc821eb624339d056e873ff3cefe682733353823
Kevin Flores García <kevinfg1413@gmail.com>
2019-06-01 18:14:20 -0600

    Close to end, we've to do proofs.

--- Karatsuba.py
+++ Karatsuba.py
@@ -12,24 +12,34 @@ MAX_SIZE = 16
 
 @total_ordering
 class AbstractNum:
+
     @abstractmethod
     def __add__(self, other) : pass
+
     @abstractmethod
     def __mul__(self, other) : pass
+
     @abstractmethod
     def __pow__(self,other): pass
+
     @abstractmethod
     def multbyOneDigit(self,digits): pass
+
     @abstractmethod
     def __floordiv__(self, other) : pass
+
     @abstractmethod
     def __sub__(self, other) : pass
+
     @abstractmethod
     def __eq__(self,other) : pass
+
     @abstractmethod
     def __lt__(self, other) : pass
+
     @abstractmethod
     def __invert__(self) : pass 
+
     def operateValidator(self,other):
         """
         Raise a exception if the user tries to operate a Num or Knum with
@@ -41,6 +51,7 @@ class AbstractNum:
         """
         if(type(self)!=type(other)):
             raise Exception(f"Can't operate {self.__class__.__name__} with {other.__class__.__name__}")
+
     def creationBase(self,digits,base):
         """
         Raise a exception if a number in the digits list is equal or greater
@@ -53,18 +64,20 @@ class AbstractNum:
         for x in str(digits):
             if int(x) >= base:
                 raise Exception(f"Can't create a number with a digit of the same value or greater than base {base}")
+
 class Num(AbstractNum):
+
     def __init__(self, digits = 0, b = Default_base, is_comple = False):
         """
         Initialize a Num object with digits, base and complement
         The Num with digits = 345 has the next form 
-        --> [3,4,5] 
+        --> [0,...,0,3,4,5] 
         """
         self.base = b
         self.is_complement = is_comple
         self.creationBase(digits,self.base)
         numberList = [int(x) for x in str(digits)]
-        if len(numberList) == 0: numberList = [1]
+        if len(numberList) == 0: numberList = [0]
         self.numDigits = len(numberList)
         if self.numDigits > MAX_SIZE:
             raise Exception("Overflow")
@@ -75,8 +88,11 @@ class Num(AbstractNum):
     
     def __str__(self):
         x = ""
-        for i in range(MAX_SIZE-len(self), MAX_SIZE):
-            x += str(self.num[i])
+        if self.is_complement : y = ~self
+        else:  y = self
+        for i in range(len(y)):
+            x += str(y[i])
+        if self.is_complement: return f"({type(self).__name__})(base = {self.base})(-{x})"
         return f"({type(self).__name__})(base = {self.base})({x})"
         
     def __repr__(self):
@@ -88,7 +104,7 @@ class Num(AbstractNum):
     def __getitem__(self, key):
         if key > self.numDigits:
             raise Exception("Overflow")
-        return self.num[(MAX_SIZE - 1) - (len(self) - key)]
+        return self.num[MAX_SIZE - (len(self) - key)]
 
     def __add__(self, other):
         """Returns the sum of two Nums 
@@ -103,6 +119,13 @@ class Num(AbstractNum):
         if self.base != other.base:
             raise Exception("Different Bases")
         else:
+            # Verifying sums -> "~Num + Num", "Num + ~Num", "~Num + ~Num"
+            if self.is_complement and other.is_complement: return ~(~self + ~other)
+            elif self.is_complement and not other.is_complement:
+                if ~self > other: return ~(~other - self)
+            elif not self.is_complement and other.is_complement:
+                if self < ~other: return ~(~self - other)
+            #-------------------------------------------------------------
             result, result_num = [], 0
             llevo = False
             for x in range(MAX_SIZE-1, -1, -1):
@@ -133,6 +156,11 @@ class Num(AbstractNum):
         if self.base != other.base:
             raise Exception("Different Bases")
         else:
+            # Verifying multiplications -> "~Num * Num", "Num * ~Num", "~Num * ~Num"
+            if self.is_complement and not other.is_complement: return ~(~self*other)
+            elif not self.is_complement and other.is_complement: return ~(self*~other)
+            elif self.is_complement and other.is_complement: return ~self*~other
+            #-----------------------------------------------------------------------
             result = []
             llevo = False
             total = type(self)(digits = 0, b = self.base)
@@ -145,9 +173,9 @@ class Num(AbstractNum):
                     sum = self.num[y] * other.num[x]
                     if llevo: sum += carry 
                     if sum >= self.base:
-                        result.append(sum - (self.base * (sum//10)))
+                        result.append(sum - (self.base * (sum//self.base)))
                         llevo = True
-                        carry = sum // 10
+                        carry = sum // self.base
                     else:
                         result.append(sum)
                         llevo = False
@@ -157,10 +185,29 @@ class Num(AbstractNum):
             return total
 
     def __floordiv__(self, other):
+        """Returns the division of two Nums 
+         -Example
+
+        [1,0] // [2]
+        ______________
+
+          [1,0]  | [2]
+                 |______
+        -[2]*[5] | [5]
+        _________|
+           [0]   |
+        _________
+        return [5]
+        """ 
         Num.operateValidator(self,other)
         if self.base != other.base:
             raise Exception("Different Bases")
         else:
+            # Verifying divisions -> "~Num // Num", "Num // ~Num", "~Num // ~Num"
+            if self.is_complement and not other.is_complement: return ~(~self//other)
+            elif not self.is_complement and other.is_complement: return ~(self//~other)
+            elif self.is_complement and other.is_complement: return ~self//~other
+            #---------------------------------------------------------------------
             result, num, enter = [0], '', False
             for x in range(MAX_SIZE - self.numDigits, MAX_SIZE):
                 num += str(self.num[x])
@@ -217,6 +264,7 @@ class Num(AbstractNum):
         for i in  range (exponent-1):
             result = result*self
         return result
+
     def multbyOneDigit(self,digit):
         """
         Multiply a Num by a int
@@ -231,7 +279,7 @@ class Num(AbstractNum):
     def __invert__(self):   
         x = [ self.base - 1 - x for x in self.num ]
         result = type(self)("".join(str(i) for i in x), self.base) + type(self)(1, self.base)
-        result.is_complement = True
+        result.is_complement = False if self.is_complement else True
         return result
 
     def __eq__(self, other):
@@ -259,68 +307,89 @@ class Num(AbstractNum):
         return True
 
     def __lshift__(self, position):
-        return self * type(self)(digits = 10**position, b =self.base)
+        # using multiplication
+        # return self * type(self)(digits = 10**position, b =self.base)
+        # To avoid calling the multiplication
+        self.num.extend([0]*position)
+        for x in range(position) :  self.num.pop(0) 
+        self.numDigits += position
+        return self
 
     def __rshift__(self, position):
-        return self // type(self)(digits = 10**position, b = self.base)
+        # using division
+        # return self // type(self)(digits = 10**position, b = self.base)
+        # To avoid calling the division
+        for x in range(position):  
+            self.num.pop()
+            self.num.insert(0,0) 
+        self.numDigits -= position
+        return self
 
 class Knum(Num):
-    """
     def __mul__(self, other):
+        """Returns the multiplication of two Knums 
+         -Example
+
+        * [1,3]
+          [4,5]
+        _________
+
+        n = len(Longer Num)//2 = 2//2 = 1
+        a = [1], b = [3], c = [4], d = [5]
+
+        ac = a * c = [1]*[4] = [4]
+        bd = b * d = [3]*[5] = [1,5]
+        ad + bc = ( a+b )*( c+d ) - ac - bd
+                = ( [1]+[3] )*( [4]+[5] ) - [4] - [1,5]
+                = [1,7]
+        
+        (ac << 2*n) + (ad + bc << n) + bd
+        = [4,0,0] + [1,7,0] + [1,5]
+        ___________________________
+                [5,8,5]
+        """ 
+        Knum.operateValidator(self, other)
         # BASE CASE
-        if len(self) <= 1 and len(other) <= 1:  
-            return type(self)(digits = self[1]*other[1], b = self.base)
+        if len(self) == 1 and len(other) == 1: 
+            return super().__mul__(other)
         else:
+            # Verifying multiplications -> "~Knum * Knum", "Knum * ~Num", "~Knum * ~Knum"
+            if self.is_complement and not other.is_complement: return ~(~self*other)
+            elif not self.is_complement and other.is_complement: return ~(self*~other)
+            elif self.is_complement and other.is_complement: return ~self*~other
+
             # CALCULADING N/2 = NUMER OF DIGITS/2
-            n = len(self) // 2 if len(self) >= len(other) else len(other) // 2
-            #if n % 2 != 0 : n+=1
-            # CALCULADING MIDDLE OF THE KNUMBER's
-            mid_self = MAX_SIZE - len(self) // 2
-            mid_other = MAX_SIZE - len(other) // 2
+            Lself = len(self)
+            Lother = len(other)
+            n = Lself // 2 + Lself % 2 if Lself >= Lother else Lother // 2 + Lother % 2
+
+            # MIDDLE OF THE NUMBERS
+            if Lself == Lother:
+                mid_self = MAX_SIZE - Lself + n
+                mid_other = MAX_SIZE - Lother + n
+            elif Lself > Lother:
+                mid_self = MAX_SIZE - Lself + (Lself - n) 
+                mid_other = MAX_SIZE - Lother
+            else: 
+                mid_self = MAX_SIZE - Lself 
+                mid_other = MAX_SIZE - Lother + (Lother - n) 
+
             # CREATING NEW KNUMBER's FROM THE PREVIOUS KNUMBER'S 
-            a = type(self)("".join(str(i) for i in self.num[MAX_SIZE-len(self):mid_self]), self.base)
+            a = type(self)("".join(str(i) for i in self.num[MAX_SIZE-Lself:mid_self]), self.base)
             b = type(self)("".join(str(i) for i in self.num[mid_self:]), self.base)
-            c = type(other)("".join(str(i) for i in other.num[MAX_SIZE-len(other):mid_other]), other.base)
+            c = type(other)("".join(str(i) for i in other.num[MAX_SIZE-Lother:mid_other]), other.base)
             d = type(other)("".join(str(i) for i in other.num[mid_other:]), other.base)
+
             # RECURSIVE CALL's (3 MULTIPLICATION's INSTEAD OF 4)
             ac = a*c
             bd = b*d
-        return ac + bd
-    """
+            ad_bc = (a+b)*(c+d) - ac - bd
+
+        # END
+        return (ac << 2*n) + (ad_bc << n) + bd
+
 if __name__=="__main__":
     
-    print("-------- PRUEBAS INICIALES --------\n")
-    print("\nSUMAS \n")
-    
-    x = 123
-    y = "200"
-    base3, base4, base7, base9, base10 = 3, 4, 7, 9, 10
-    """ a = Num(x, base10)
-    b = Num(y, base10)
-    c = Num(x, base4)
-    d = Num(y, base4)
-    e = Knum(x, base7)
-    f = Knum(y, base7)
-    g = Knum(x, base9)
-    h = Knum(y, base9)"""
-    i = Num(x,10)
-    j = Num(y,10)
-    b = Num(123, base10)
-    c = Num(123, base10)
-    o = 12
-    """  print(f"a = {a}")
-    print(f"b = {b}")
-    print(f"d = {d}")
-    print(f"c = {c}")
-    print(f"e = {e}")
-    print(f"f = {f}")
-    print(f"g = {g}")
-    print(f"h = {h}")
-    print(f"a+b = {a+b}")
-    print(f"c+d = {a+b}")
-    print(f"e+f = {a+b}")
-    print(f"g+h = {a+b}")
-    print(f"i+j = {i+j}")
-    print(f"i+j = {i+j}")"""
-    print(i-j)
-    print(i.multbyOneDigit(3))
+    i = Knum(12,5)
+    j = Knum("310",5)
+    print(i*j)
